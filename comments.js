2913c52231a7af101c53ad274d218a3d70b61c4d
Jasael <67719321+jasael@users.noreply.github.com>
2024-12-18 05:08:02 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// Create web server
+// Create a new express application
+const express = require('express');
+const app = express();
+
+// Create a new comment
+app.post('/comments', (req, res) => {
+  res.send('Create a new comment');
+});
+
+// Read all comments
+app.get('/comments', (req, res) => {
+  res.send('Read all comments');
+});
+
+// Read a comment
+app.get('/comments/:id', (req, res) => {
+  res.send('Read a comment');
+});
+
+// Update a comment
+app.put('/comments/:id', (req, res) => {
+  res.send('Update a comment');
+});
+
+// Delete a comment
+app.delete('/comments/:id', (req, res) => {
+  res.send('Delete a comment');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started');
+});
